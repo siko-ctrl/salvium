edfbf595d5bf4f90d9b0f003fa49c1a187e405b2
siko-ctrl <ttht124@gmail.com>
2024-12-20 16:35:19 +0200

    Major optimization  Fix

---
+++ _site/js/image-fallback.js
@@ -0,0 +1,15 @@
+// Check WebP support
+function checkWebP(callback) {
+    var webP = new Image();
+    webP.onload = webP.onerror = function () {
+        callback(webP.height == 2);
+    };
+    webP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
+}
+
+// Add WebP class to body if supported
+checkWebP(function(support) {
+    if (support) {
+        document.body.classList.add('webp');
+    }
+});
